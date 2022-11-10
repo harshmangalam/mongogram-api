dd157dd00fb4c00de8728abd87a25a2ff78d4f1b
harshmangalam <harshdev8218@gmail.com>
2022-11-10 13:42:31 +0530

    connect with mongodb

--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoInstance struct {
+	Client *mongo.Client
+	Db     *mongo.Database
+}
+
+var mg MongoInstance
+
+const dbName = "mongogram"
+const mongoURI = "mongodb://localhost:27017/" + dbName
+
+func ConnectMongo() error {
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		return err
+	}
+
+	db := client.Database(dbName)
+
+	mg = MongoInstance{
+		Client: client,
+		Db:     db,
+	}
+
+	log.Printf("Mongodb connected (%v)", dbName)
+
+	return nil
+
+}
 
 func main() {
 
+	if err := ConnectMongo(); err != nil {
+		log.Fatal(err)
+	}
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
