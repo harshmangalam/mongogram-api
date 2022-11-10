02d283381c8f5c937bde091e20bbf46be88eaadb
harshmangalam <harshdev8218@gmail.com>
2022-11-10 20:04:37 +0530

    added user model

---
+++ models/user.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	Id        primitive.ObjectID `bson:"_id", json:"_id"`
+	Name      string             `bson:"name",json:"name"`
+	Username  string             `bson:"username",json:"username"`
+	Password  string             `bson:"password",json:"password"`
+	Email     string             `bson:"email",json:"email"`
+	Phone     string             `bson:"phone",json:"phone"`
+	IsActive  bool               `bson:"isActive",json:"isActive"`
+	CreatedAt *time.Time         `bson:"createdAt",json:"createdAt"`
+	UpdatedAt *time.Time         `bson:"updatedAt",json:"updatedAt"`
+}
