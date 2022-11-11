834c52160fca99888e4ddeeaffd4e5adf4b7b15d
harshmangalam <harshdev8218@gmail.com>
2022-11-11 11:17:48 +0530

    added database package

--- database/connect.go
+++ database/connect.go
@@ -1,4 +1,4 @@
-package utils
+package database
 
 import (
 	"context"
@@ -7,13 +7,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-type MongoInstance struct {
-	Client *mongo.Client
-	Db     *mongo.Database
-}
-
-var Mi MongoInstance
 
 const dbName = "mongogram"
 const mongoURI = "mongodb://localhost:27017/" + dbName
---
+++ database/database.go
@@ -0,0 +1,10 @@
+package database
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type MongoInstance struct {
+	Client *mongo.Client
+	Db     *mongo.Database
+}
+
+var Mi MongoInstance
