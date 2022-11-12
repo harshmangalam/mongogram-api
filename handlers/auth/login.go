829235d4e45531231eacc37d4ef4e48e8b50cbd8
harshmangalam <harshdev8218@gmail.com>
2022-11-12 09:48:41 +0530

    verify username

--- handlers/auth/login.go
+++ handlers/auth/login.go
@@ -1,9 +1,14 @@
 package auth
 
 import (
+	"context"
+	"mongogram/database"
+	"mongogram/models"
 	"mongogram/utils"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type LoginBody struct {
@@ -35,8 +40,29 @@ func Login(c *fiber.Ctx) error {
 		})
 
 	}
+
+	user := new(models.User)
+	usersColl := database.Mi.Db.Collection(database.UsersCollection)
+
 	// verify user email/username/phone
 
+	if err := usersColl.FindOne(context.TODO(), bson.D{{Key: "email", Value: loginBody.Username}, {Key: "username", Value: loginBody.Username}, {Key: "phone", Value: loginBody.Username}}).Decode(user); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"status":  "error",
+				"message": "Invalid credentials",
+				"data":    errors,
+			})
+		} else {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"status":  "error",
+				"message": err.Error(),
+				"data": fiber.Map{
+					"fields": loginBody,
+				},
+			})
+		}
+	}
 	// match user password
 
 	// update user active status
