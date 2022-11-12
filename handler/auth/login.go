d3d0f33ecd7bdc530f45e12a71785e11af8815f4
harshmangalam <harshdev8218@gmail.com>
2022-11-12 09:28:14 +0530

    added validations in login handler

--- handler/auth/login.go
+++ handler/auth/login.go
@@ -1,8 +1,39 @@
 package auth
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"mongogram/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type LoginBody struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
 
 func Login(c *fiber.Ctx) error {
+
+	loginBody := new(LoginBody)
+
+	if err := c.BodyParser(loginBody); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	// validate users input
+	errors := utils.ValidateStruct(loginBody)
+	if errors != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(errors)
+
+	}
+	// verify user email/username/phone
+
+	// match user password
+
+	// update user active status
+
+	// create jwt token
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"status":  "success",
