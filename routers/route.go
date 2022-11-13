429adeb95b68660bc7cf13ddc1a4424a801d8588
harshmangalam <harshdev8218@gmail.com>
2022-11-13 10:20:22 +0530

    added protected route

--- routers/route.go
+++ routers/route.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"mongogram/handlers/auth"
 	"mongogram/handlers/user"
+	"mongogram/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -18,5 +19,5 @@ func SetupRoute(app *fiber.App) {
 	// user route
 
 	userRoute := app.Group("/user")
-	userRoute.Get("/me", user.GetMe)
+	userRoute.Get("/me", middlewares.Protected(), user.GetMe)
 }
