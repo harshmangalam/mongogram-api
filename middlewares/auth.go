b7a6fbd8f3b5e53c632425ff65a05ee9252f8bef
harshmangalam <harshdev8218@gmail.com>
2022-11-13 10:20:06 +0530

    added success token parse handler

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -3,21 +3,31 @@ package middlewares
 import (
 	"github.com/gofiber/fiber/v2"
 	jwtware "github.com/gofiber/jwt/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 // Protected protect routes
 func Protected() fiber.Handler {
 	return jwtware.New(jwtware.Config{
-		SigningKey:   []byte("SECRET"),
-		ErrorHandler: jwtError,
+		SigningKey:     []byte("SECRET"),
+		ErrorHandler:   jwtError,
+		SuccessHandler: success,
+		ContextKey:     "jwtToken",
 	})
 }
 
 func jwtError(c *fiber.Ctx, err error) error {
 	if err.Error() == "Missing or malformed JWT" {
-		return c.Status(fiber.StatusBadRequest).
-			JSON(fiber.Map{"status": "error", "message": "Missing or malformed JWT", "data": nil})
+		c.Next()
 	}
 	return c.Status(fiber.StatusUnauthorized).
 		JSON(fiber.Map{"status": "error", "message": "Invalid or expired JWT", "data": nil})
+}
+
+func success(c *fiber.Ctx) error {
+	token := c.Locals("jwtToken").(*jwt.Token)
+	claims := token.Claims.(jwt.MapClaims)
+	userId := claims["userId"]
+	c.Locals("userId", userId)
+	return c.Next()
 }
