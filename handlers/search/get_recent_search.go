a902389c1ce9a8f20506639ed4a7a372bc293970
harshmangalam <harshdev8218@gmail.com>
2022-11-15 14:48:16 +0530

    fetch recent searches

---
+++ handlers/search/get_recent_search.go
@@ -0,0 +1,45 @@
+package search
+
+import (
+	"context"
+	"mongogram/database"
+	"mongogram/models"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func GetRecentSearche(c *fiber.Ctx) error {
+	userId := c.Locals("userId")
+
+	searchColl := database.Mi.Db.Collection(database.SearchCollection)
+
+	var searches []models.Search
+	filter := bson.D{
+		{"userId", userId},
+	}
+	cursor, err := searchColl.Find(context.TODO(), filter)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  "error",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	if err = cursor.All(context.TODO(), &searches); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  "error",
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  "success",
+		"message": "fetch search results",
+		"data": fiber.Map{
+			"searches": searches,
+		},
+	})
+}
