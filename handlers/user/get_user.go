6f3d70e372442caff8e522e24853aa2a95e17ccf
harshmangalam <harshdev8218@gmail.com>
2022-11-24 10:31:06 +0530

    get user

---
+++ handlers/user/get_user.go
@@ -0,0 +1,61 @@
+package user
+
+import (
+	"context"
+	"mongogram/database"
+	"mongogram/models"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func GetUser(c *fiber.Ctx) error {
+
+	userId, _ := primitive.ObjectIDFromHex(c.Params("userId"))
+
+	usersColl := database.Mi.Db.Collection(database.UsersCollection)
+
+	user := new(models.User)
+	filter := bson.D{{Key: "_id", Value: userId}}
+
+	if err := usersColl.FindOne(context.TODO(), filter).Decode(user); err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  "error",
+			"message": "User not found",
+			"data":    nil,
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  "success",
+		"message": "Get user",
+		"data": fiber.Map{
+			"user": user,
+		},
+	})
+}
+
+func GetMe(c *fiber.Ctx) error {
+
+	userId := c.Locals("userId")
+
+	usersColl := database.Mi.Db.Collection(database.UsersCollection)
+
+	user := new(models.User)
+	filter := bson.D{{Key: "_id", Value: userId}}
+
+	if err := usersColl.FindOne(context.TODO(), filter).Decode(user); err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  "error",
+			"message": "User not found",
+			"data":    nil,
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":  "success",
+		"message": "get current user",
+		"data": fiber.Map{
+			"user": user,
+		},
+	})
+}
