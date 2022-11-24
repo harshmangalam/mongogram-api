427cc6f66236857d4deb701903a0ceb8c5b12efc
harshmangalam <harshdev8218@gmail.com>
2022-11-24 10:08:12 +0530

    added user reset pass otp

--- models/user.go
+++ models/user.go
@@ -7,15 +7,16 @@ import (
 )
 
 type User struct {
-	Id        primitive.ObjectID `bson:"_id" json:"_id"`
-	Name      string             `bson:"name" json:"name"`
-	Username  string             `bson:"username" json:"username"`
-	Password  string             `bson:"password" json:"-"`
-	Email     string             `bson:"email" json:"email"`
-	Phone     string             `bson:"phone" json:"phone"`
-	Birthday  time.Time          `bson:"birthday" json:"birthday"`
-	Bio       string             `bson:"bio" json:"bio"`
-	IsActive  bool               `bson:"isActive" json:"isActive"`
-	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
-	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
+	Id           primitive.ObjectID `bson:"_id" json:"_id"`
+	Name         string             `bson:"name" json:"name"`
+	Username     string             `bson:"username" json:"username"`
+	Password     string             `bson:"password" json:"-"`
+	Email        string             `bson:"email" json:"email"`
+	Phone        string             `bson:"phone" json:"phone"`
+	Birthday     time.Time          `bson:"birthday" json:"birthday"`
+	Bio          string             `bson:"bio" json:"bio"`
+	ResetPassOtp string             `bson:"resetPassOtp" json:"-"`
+	IsActive     bool               `bson:"isActive" json:"isActive"`
+	CreatedAt    time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt    time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
