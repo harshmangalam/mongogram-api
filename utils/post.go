33790485b5bb06377351932035fe576897b10142
harshmangalam <harshdev8218@gmail.com>
2022-11-26 10:03:37 +0530

    get post by id helper

---
+++ utils/post.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"context"
+	"mongogram/database"
+	"mongogram/models"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func FindPostById(postId any) (*models.Post, error) {
+	post := new(models.Post)
+
+	postsColl := database.Mi.Db.Collection(database.PostsCollection)
+
+	if err := postsColl.FindOne(context.TODO(), bson.D{{"_id", postId}}).Decode(post); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil
+		} else {
+			return nil, err
+		}
+	}
+
+	return post, nil
+}
