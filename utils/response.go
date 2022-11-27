8bbbf160680326132dd466a643b2fc925d02980e
harshmangalam <harshdev8218@gmail.com>
2022-11-27 12:46:44 +0530

    added Response schema and methods

--- utils/response.go
+++ utils/response.go
@@ -9,10 +9,44 @@ const (
 	Error   StatusText = "error"
 )
 
-func CustomResponse(c *fiber.Ctx, statusText string, statusCode int, message string, data any) error {
+type ResponseSchema struct {
+	Ctx        *fiber.Ctx `json:"-"`
+	StatusText StatusText `json:"status"`
+	StatusCode int        `json:"-"`
+	Message    string     `json:"message"`
+	Data       fiber.Map  `json:"data"`
+}
+
+func NewResponseSchema() *ResponseSchema {
+	return &ResponseSchema{}
+}
+
+func (r *ResponseSchema) SetStatusText(statusText StatusText) *ResponseSchema {
+	r.StatusText = statusText
+	return r
+}
+func (r *ResponseSchema) SetStatusCode(statusCode int) *ResponseSchema {
+	r.StatusCode = statusCode
+	return r
+}
+func (r *ResponseSchema) SetMessage(message string) *ResponseSchema {
+	r.Message = message
+	return r
+}
+
+func (r *ResponseSchema) SetData(data fiber.Map) *ResponseSchema {
+	r.Data = data
+	return r
+}
+
+func CustomResponse(c *fiber.Ctx, statusText StatusText, statusCode int, message string, data any) error {
 	return c.Status(statusCode).JSON(fiber.Map{
 		"status":  statusText,
 		"message": message,
 		"data":    data,
 	})
+}
+
+func InternalServerErrorResponse(c *fiber.Ctx, err error) {
+	return CustomResponse(c, Error, fiber.StatusInternalServerError, "", nil)
 }
