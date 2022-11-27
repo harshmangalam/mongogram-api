4fa14157a8d5695e1cb5ef08704063f6ed5a4d15
harshmangalam <harshdev8218@gmail.com>
2022-11-27 12:34:27 +0530

    added response status text

--- utils/response.go
+++ utils/response.go
@@ -2,6 +2,13 @@ package utils
 
 import "github.com/gofiber/fiber/v2"
 
+type StatusText string
+
+const (
+	Success StatusText = "success"
+	Error   StatusText = "error"
+)
+
 func CustomResponse(c *fiber.Ctx, statusText string, statusCode int, message string, data any) error {
 	return c.Status(statusCode).JSON(fiber.Map{
 		"status":  statusText,
