ff1d8ae5002d094575d7be7e75236d3c9e7bfd3e
harshmangalam <harshdev8218@gmail.com>
2022-11-27 14:24:19 +0530

    reset password

--- handlers/accounts/reset_password.go
+++ handlers/accounts/reset_password.go
@@ -16,48 +16,45 @@ func ResetPassword(c *fiber.Ctx) error {
 	body := new(ResetPasswordBody)
 	// parse request body
 	if err := c.BodyParser(body); err != nil {
-		return utils.ReturnError(c, fiber.StatusInternalServerError, err.Error(), nil)
+		return utils.InternalServerErrorResponse(c, err)
 	}
 
 	// validate input body
 
 	errors := utils.ValidateStruct(body)
 	if errors != nil {
-		return utils.ReturnError(c, fiber.StatusBadRequest, "Invalid input", errors)
+		return utils.UnprocessedInputResponse(c, fiber.Map{"errors": errors})
 
 	}
 
 	// verify user account
 	user, err := utils.FindUser("email", body.Email)
-
 	if err == nil && user == nil {
-		return utils.ReturnError(c, fiber.StatusNotFound, "User not found", nil)
+		return utils.NotFoundErrorResponse(c)
 	}
 	if err != nil {
-		return utils.ReturnError(c, fiber.StatusInternalServerError, err.Error(), nil)
+		return utils.InternalServerErrorResponse(c, err)
 	}
+
 	// send otp
 	pin, err := utils.GeneratePin(6)
 	if err != nil {
-		return utils.ReturnError(c, fiber.StatusInternalServerError, err.Error(), nil)
+		return utils.InternalServerErrorResponse(c, err)
 	}
 
+	// save pin in db
 	_, err = utils.UpdateUser(user.Id, bson.M{"resetPassOtp": pin})
 
 	if err != nil {
-		return utils.ReturnError(c, fiber.StatusInternalServerError, err.Error(), nil)
+		return utils.InternalServerErrorResponse(c, err)
 	}
 	message := []byte("To: " + body.Email + "\r\n" +
 		"Subject: Reset your password!\r\n" +
 		"\r\n" + "Your 6 digit pin code is " + pin +
 		"\r\n")
 	if err := utils.SendMail([]string{body.Email}, config.Config("SMTP_USERNAME"), message); err != nil {
-		return utils.ReturnError(c, fiber.StatusInternalServerError, err.Error(), nil)
+		return utils.InternalServerErrorResponse(c, err)
 	}
 
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"status":  "success",
-		"message": "Send otp on your email",
-		"data":    nil,
-	})
+	return utils.OkResponse(c, "Send otp in your email", nil)
 }
