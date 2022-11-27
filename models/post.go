395c1935c6c721eab0b709d4a0db8753f76a0b66
harshmangalam <harshdev8218@gmail.com>
2022-11-27 12:23:28 +0530

    added media id in model

--- models/post.go
+++ models/post.go
@@ -8,8 +8,7 @@ import (
 
 type Post struct {
 	Id        primitive.ObjectID `bson:"_id" json:"_id"`
-	MediaType string             `bson:"mediaType" json:"mediaType"`
-	MediaUrl  string             `bson:"mediaUrl" json:"mediaUrl"`
+	MediaId   primitive.ObjectID `bson:"mediaId" json:"mediaId"`
 	Content   string             `bson:"content" json:"content"`
 	AuthorId  primitive.ObjectID `bson:"authorId" json:"authorId"`
 	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
