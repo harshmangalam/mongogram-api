b5f018e60c4353a07cbb9e6b8cbaa055e02aadf4
harshmangalam <harshdev8218@gmail.com>
2022-12-04 14:41:26 +0530

    added api

--- routers/route.go
+++ routers/route.go
@@ -7,7 +7,6 @@ import (
 	"mongogram/handlers/post"
 	"mongogram/handlers/search"
 	"mongogram/handlers/user"
-
 	"mongogram/middlewares"
 
 	"github.com/gofiber/fiber/v2"
@@ -16,25 +15,25 @@ import (
 func SetupRoute(app *fiber.App) {
 
 	// auth route
-	authRoute := app.Group("/auth")
+	authRoute := app.Group("/api/auth")
 	authRoute.Post("/signup", auth.Signup)
 	authRoute.Post("/login", auth.Login)
 
 	// user route
-	userRoute := app.Group("/user")
+	userRoute := app.Group("/api/user")
 	userRoute.Get("/me", middlewares.Protected(), user.GetMe)
 	userRoute.Get("/:userId", user.GetUser)
 	userRoute.Post("/:userId/friendship", middlewares.Protected(), user.Friendship)
 
 	// search route
-	searchRoute := app.Group("/search")
+	searchRoute := app.Group("/api/search")
 	searchRoute.Get("/", middlewares.Protected(), search.AtlasSearch)
 	searchRoute.Get("/recent", middlewares.Protected(), search.GetRecentSearch)
 	searchRoute.Delete("/recent", middlewares.Protected(), search.DeleteRecentSearchs)
 	searchRoute.Delete("/recent/:searchId", middlewares.Protected(), search.DeleteRecentSearch)
 
 	// account route
-	accountRoute := app.Group("/account")
+	accountRoute := app.Group("/api/account")
 	accountRoute.Put("/edit", middlewares.Protected(), accounts.EditAccount)
 	accountRoute.Patch("/change_password", middlewares.Protected(), accounts.ChangePassword)
 	accountRoute.Post("/reset_password", accounts.ResetPassword)
@@ -42,13 +41,13 @@ func SetupRoute(app *fiber.App) {
 	accountRoute.Post("/deactivate_temporary", middlewares.Protected(), accounts.DeactivateAccountTemporary)
 
 	//posts
-	postRoute := app.Group("/post")
+	postRoute := app.Group("/api/post")
 	postRoute.Post("/", middlewares.Protected(), post.CreatePost)
 	postRoute.Get("/", post.GetPosts)
 	postRoute.Get("/:postId", post.GetPost)
 
 	// media
-	mediaRoute := app.Group("/media")
+	mediaRoute := app.Group("/api/media")
 	mediaRoute.Post("/upload", middlewares.Protected(), media.Upload)
 	mediaRoute.Get("/", media.GetMedia)
 
